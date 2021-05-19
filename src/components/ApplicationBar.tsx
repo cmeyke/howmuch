5996eb30d3c46816230110e33eb34ea4bf592e65
Carsten Meyke <carsten.meyke@gmail.com>
2021-05-19 18:51:22 +0200

    address abbreviation, correctness test and checksum formatting

--- src/components/ApplicationBar.tsx
+++ src/components/ApplicationBar.tsx
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Button from '@material-ui/core/Button'
 import Home from '@material-ui/icons/Home'
+import { ethers } from 'ethers'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,7 +17,8 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     toolbarButtons: {
       borderRadius: 16,
-      marginLeft: 'auto'
+      marginLeft: 'auto',
+      textTransform: 'none'
     }
   })
 )
@@ -31,6 +33,14 @@ export default function ApplicationBar ({
   setAddress
 }: ApplicationBarType) {
   const classes = useStyles()
+  let displayAddress = ''
+  if (ethers.utils.isAddress(address)) {
+    displayAddress = ethers.utils.getAddress(address)
+    displayAddress = `${displayAddress.slice(0, 6)}...${displayAddress.slice(
+      address.length - 4,
+      address.length
+    )}`
+  }
 
   return (
     <div className={classes.root}>
@@ -52,7 +62,7 @@ export default function ApplicationBar ({
               aria-label='address'
               onClick={() => setAddress('change')}
             >
-              {address}
+              {displayAddress ? displayAddress : 'Connect a wallet'}
             </Button>
           ) : (
             <div></div>
