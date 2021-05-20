e7ed8c9a6bae226320b2d2a98c842e2133b5e162
Carsten Meyke <carsten.meyke@gmail.com>
2021-05-20 10:29:44 +0200

    deletet debug log message

--- src/components/GetAssest.tsx
+++ src/components/GetAssest.tsx
@@ -97,7 +97,6 @@ export const GetAssets = ({
               )
               .then(res => {
                 validatorBalance[3] = res.data.data.attestation_efficiency
-                console.log(res.data.data.attestation_efficiency)
                 setValidatorBalances(v => v.concat([validatorBalance]))
               })
               .catch(err => {
