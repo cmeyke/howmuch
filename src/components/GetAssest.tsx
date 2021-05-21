5ffc408bbab3d71f32697c3ed7a229ab4be17aa7
Carsten Meyke <carsten.meyke@gmail.com>
2021-05-21 17:31:03 +0200

    migrated to try ... catch

--- src/components/GetAssest.tsx
+++ src/components/GetAssest.tsx
@@ -1,121 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Web3 from 'web3'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Web3 from "web3";
 
 type GetAssetsParameterType = {
-  address: string
-  setPriceEUR: React.Dispatch<React.SetStateAction<number>>
-  setBalance: React.Dispatch<React.SetStateAction<number>>
+  address: string;
+  setPriceEUR: React.Dispatch<React.SetStateAction<number>>;
+  setBalance: React.Dispatch<React.SetStateAction<number>>;
   setValidatorBalances: React.Dispatch<
     React.SetStateAction<[number, number, number, number][]>
-  >
-}
+  >;
+};
 
 export const GetAssets = ({
   address,
   setPriceEUR,
   setBalance,
-  setValidatorBalances
+  setValidatorBalances,
 }: GetAssetsParameterType) => {
-  const validatorsInitialValue: number[] = []
-  const [validators, setValidators] = useState(validatorsInitialValue)
+  const validatorsInitialValue: number[] = [];
+  const [validators, setValidators] = useState(validatorsInitialValue);
 
-  function getPriceEUR () {
+  async function getPriceEUR() {
     // console.log('getPriceEUR')
-    axios
-      .get('https://api.kraken.com/0/public/Ticker?pair=ETHEUR')
-      .then(res => {
-        setPriceEUR(res.data.result.XETHZEUR.c[0])
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    try {
+      const res = await axios.get(
+        "https://api.kraken.com/0/public/Ticker?pair=ETHEUR"
+      );
+      setPriceEUR(res.data.result.XETHZEUR.c[0]);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  function getNewProvider () {
+  function getNewProvider() {
     // console.log('getNewProvider')
     if (Web3.givenProvider === null) {
       const url =
-        'https://eth-mainnet.alchemyapi.io/v2/8j-Eu5zxTrvO6_WrCBu3iVuOR7jtC7EV'
-      return new Web3(url)
+        "https://eth-mainnet.alchemyapi.io/v2/8j-Eu5zxTrvO6_WrCBu3iVuOR7jtC7EV";
+      return new Web3(url);
     } else {
-      return new Web3(Web3.givenProvider)
+      return new Web3(Web3.givenProvider);
     }
   }
 
-  const web3 = getNewProvider()
+  const web3 = getNewProvider();
 
-  function getBalance () {
+  async function getBalance() {
     // console.log('getBalance')
-    web3.eth
-      .getBalance(address)
-      .then(balance => {
-        setBalance(Number(Web3.utils.fromWei(balance)))
-      })
-      .catch(err => {
-        setBalance(0)
-        console.log(err)
-      })
+    try {
+      const balance = await web3.eth.getBalance(address);
+      setBalance(Number(Web3.utils.fromWei(balance)));
+    } catch (err) {
+      setBalance(0);
+      console.error(err);
+    }
+  }
+
+  async function getValidators() {
+    try {
+      const res = await axios.get(
+        `https://beaconcha.in/api/v1/validator/eth1/${address}`
+      );
+      setValidators(
+        res.data.data.map(
+          (item: { validatorindex: number }) => item.validatorindex
+        )
+      );
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   useEffect(() => {
-    if (address && address !== 'change') {
-      getBalance()
-      getPriceEUR()
-      // console.log('useEffect: get validators')
-      axios
-        .get(`https://beaconcha.in/api/v1/validator/eth1/${address}`)
-        .then(res => {
-          setValidators(
-            res.data.data.map(
-              (item: { validatorindex: number }) => item.validatorindex
-            )
-          )
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    if (address && address !== "change") {
+      getBalance();
+      getPriceEUR();
+      getValidators();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [address])
+  }, [address]);
+
+  async function getValidatorBalances(validator: number) {
+    try {
+      const validatorData = await axios.get(
+        `https://beaconcha.in/api/v1/validator/${validator}`
+      );
+      const validatorEfficiency = await axios.get(
+        `https://beaconcha.in/api/v1/validator/${validator}/attestationefficiency`
+      );
+      const validatorBalance: [number, number, number, number] = [
+        validator,
+        validatorData.data.data.balance / 1000000000,
+        validatorData.data.data.effectivebalance / 1000000000,
+        100 -
+          (validatorEfficiency.data.data.attestation_efficiency * 100 - 100),
+      ];
+      setValidatorBalances((v) => v.concat([validatorBalance]));
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   useEffect(() => {
     if (address) {
       // console.log('useEffect: get validator balances')
-      setValidatorBalances([])
+      setValidatorBalances([]);
 
-      validators.forEach(validator => {
-        axios
-          .get(`https://beaconcha.in/api/v1/validator/${validator}`)
-          .then(res => {
-            const validatorBalance: [number, number, number, number] = [
-              validator,
-              res.data.data.balance / 1000000000,
-              res.data.data.effectivebalance / 1000000000,
-              0
-            ]
-            axios
-              .get(
-                `https://beaconcha.in/api/v1/validator/${validator}/attestationefficiency`
-              )
-              .then(res => {
-                validatorBalance[3] =
-                  100 - (res.data.data.attestation_efficiency * 100 - 100)
-                setValidatorBalances(v => v.concat([validatorBalance]))
-              })
-              .catch(err => {
-                console.log(err)
-              })
-          })
-          .catch(err => {
-            console.log(err)
-          })
-      })
+      validators.forEach((validator) => {
+        getValidatorBalances(validator);
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [validators])
+  }, [validators]);
 
   // console.log('GetAssets')
 
-  return <div></div>
-}
+  return <div></div>;
+};
